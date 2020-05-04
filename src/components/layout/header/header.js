6f028101bc85efba070acd22e498f41977246e59
Matthew Phillips <mphillips@xogrp.com>
2020-05-03 19:29:14 -0500

    Add Paypal donate button

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -11,9 +11,19 @@ const Header = ({ menuLinks }) => (
       style={{
         background: `#3b3b3b`,
         height: 81,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
       }}
     >
       <BannerImage/>
+      <form style={{marginTop: 16}} action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="N4KGP7JPWMSCE" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+
     </div>
       <nav className="nav">
       {
